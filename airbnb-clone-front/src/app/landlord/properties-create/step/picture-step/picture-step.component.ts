1f2bbf9daf95751038b043a2d0fb9e8fd3cd3c75
lcs-1 <lcsx1945@gmail.com>
2025-10-10 10:39:11 +0530

    Pictures uploading front end updates

---
+++ airbnb-clone-front/src/app/landlord/properties-create/step/picture-step/picture-step.component.ts
@@ -0,0 +1,53 @@
+import { Component, EventEmitter, input } from '@angular/core';
+import { NewListingPicture } from '../../../model/picture.model';
+import { InputTextModule } from "primeng/inputtext";
+
+@Component({
+  selector: 'app-picture-step',
+  standalone: true,
+  imports: [InputTextModule],
+  templateUrl: './picture-step.component.html',
+  styleUrl: './picture-step.component.scss'
+})
+export class PictureStepComponent {
+  pictures = input.required<Array<NewListingPicture>>();
+
+  pictureChange = new EventEmitter<Array<NewListingPicture>>();
+
+  stepValidityChange = new EventEmitter<boolean>();
+
+  onUploadNewPicture(target: EventTarget | null){
+    const picturesFileList = this.extractFileFromTarget(target);
+    if(picturesFileList!=null){
+      for(let i=0;i<picturesFileList.length;i++){
+        const picture = picturesFileList[i];
+        if(picture!==null){
+          const displayPicture: NewListingPicture = {
+            file: picture,
+            urlDisplay: URL.createObjectURL(picture)
+          }
+          this.pictures().push(displayPicture);
+        }
+
+      }
+      this.pictureChange.emit(this.pictures());
+      this.validatePictures();
+    }
+  }
+
+  validatePictures() {
+    if(this.pictures().length>=5){
+      this.stepValidityChange.emit(true);
+    } else{
+      this.stepValidityChange.emit(false);
+    }
+  }
+
+  extractFileFromTarget(target: EventTarget | null) {
+    const htmlInputtarget = target as HTMLInputElement;
+    if(target==null || htmlInputtarget.files==null){
+      return null;
+    }
+    return htmlInputtarget.files;
+  }
+}
